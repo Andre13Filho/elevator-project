ae8722f6fc3de980750a1b2a4b5dcf0f7721ae54
OtavioAugusto27 <otavio.augusto.rosa1@gmail.com>
2025-10-18 17:32:41 -0300

    Classe elevador

---
+++ model/elevator.java
@@ -0,0 +1,80 @@
+package model;
+
+public class Elevador {
+    private int andarAtual = 0;
+    private int totalAndares;
+    private int capacidade;
+    private int pessoasPresentes = 0;
+    private boolean emManutencao = false;
+
+    public void inicializar(int capacidade, int totalAndares) {
+        if (capacidade <= 0 || totalAndares < 0) {
+            throw new IllegalArgumentException("Valores inválidos");
+        }
+        this.capacidade = capacidade;
+        this.totalAndares = totalAndares;
+        this.andarAtual = 0;
+        this.pessoasPresentes = 0;
+        this.emManutencao = false;
+    }
+
+    public boolean entrar() {
+        if (emManutencao || pessoasPresentes >= capacidade) {
+            return false;
+        }
+        pessoasPresentes++;
+        return true;
+    }
+
+    public boolean sair() {
+        if (emManutencao || pessoasPresentes <= 0) {
+            return false;
+        }
+        pessoasPresentes--;
+        return true;
+    }
+
+    public boolean subir() {
+        if (emManutencao || andarAtual >= totalAndares) {
+            return false;
+        }
+        andarAtual++;
+        return true;
+    }
+
+    public boolean descer() {
+        if (emManutencao || andarAtual <= 0) {
+            return false;
+        }
+        andarAtual--;
+        return true;
+    }
+
+    public void colocarEmManutencao() {
+        emManutencao = true;
+    }
+
+    public void liberarManutencao() {
+        emManutencao = false;
+    }
+
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+
+    public int getPessoasPresentes() {
+        return pessoasPresentes;
+    }
+
+    public int getTotalAndares() {
+        return totalAndares;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public boolean isEmManutencao() {
+        return emManutencao;
+    }
+}
