2ca5f3afed199c396325db66492606580a73791a
Luana <129308460+Lu4-4n4@users.noreply.github.com>
2025-10-19 16:35:38 -0300

    feat: classe predio

---
+++ model/Predio.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.List;
+
+public class Predio{
+    private String nome;
+    private List<Elevador> elevadores;
+
+    public Predio(String nome) {
+        this.nome = nome;
+        this.elevadores = null;
+    }
+    public void adicionarElevador(Elevador elevador){
+        if(elevador == null){
+            throw new NullPointerException("Erro ao adicionar elevador");
+        }
+        elevadores.add(elevador);
+    }
+    public Elevador getElevador(int indice){
+        if(indice<0 || indice>elevadores.size()){
+            throw new IndexOutOfBoundsException("Indice inválido");
+        }
+        return elevadores.get(indice);
+    }
+    public int getQuantidadeElevadores(){
+        return elevadores.size();
+    }
+    public String getNome(){
+        return nome;
+    }
+}
